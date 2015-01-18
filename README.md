6df74b5eef4e141d9ff3e783c345195119c26f2e
mostofa62 <golammostofa31188@gmail.com>
2015-01-18 18:32:06 +0600

    First Commit of Angular Dyanmic View

---
+++ README.md
@@ -0,0 +1,1 @@
+# angular-dynamic-view-and-framework
---
+++ app2/app/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['ngRoute','angular-responsive','door3.css'])
+    .config(function($routeProvider, responsiveHelperProvider) {
+        var device = 'desktop';
+		var maincss='/bootstrap/css/bootstrap.min.css';
+        var responsiveHelper = responsiveHelperProvider.$get();
+        if(responsiveHelper.isMobile()){
+            device = 'mobile';
+			maincss= '/uikit/css/uikit.min.css';
+        }
+		 if(responsiveHelper.isTablet()){
+            device = 'tablet';
+        }
+	
+        $routeProvider.when('/view1', {templateUrl: device + '/partial1.html', controller: 'myController',css:[device+maincss,device +'/style.css']});
+        $routeProvider.when('/view2', {templateUrl: device + '/partial2.html', controller: 'myController',css:[device+maincss,device +'/style.css']});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    });
+
+
+function myController($scope) {
+    $scope.email = 'jlavin@jimlavin.net';
+}
